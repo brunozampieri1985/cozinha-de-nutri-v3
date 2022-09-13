690e1f8f5bb0261ab18eb6235d097c99c3053b2a
brunozampieri1985 <brunozampieri1985@gmail.com>
2022-09-13 14:57:57 -0300

    Fix: added passHref property at LinkComponent

--- src/lib/components/Socials.tsx
+++ src/lib/components/Socials.tsx
@@ -6,12 +6,12 @@ const Socials: React.FC = () => {
    return (
       <div className='socials'>
          <div className='whatsapp'>
-            <Link href="https://wa.me/5513991632069" passHref target={'_blank'}>
+            <Link href="https://wa.me/5513991632069" passHref>
                <FaWhatsapp />
             </Link>
          </div>
          <div className='instagram'>
-            <Link href="https://www.instagram.com/cozinha.denutri/">
+            <Link href="https://www.instagram.com/cozinha.denutri/" passHref>
                <FaInstagram />
             </Link>
          </div>
