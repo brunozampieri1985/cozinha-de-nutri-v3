137c322ebcb2c1dd032c72b15e3c2800677387bb
brunozampieri1985 <brunozampieri1985@gmail.com>
2022-09-13 11:54:53 -0300

    fix: Service Worker and font-swap

--- next.config.js
+++ next.config.js
@@ -7,9 +7,9 @@ const nextConfig = {
 const withPWA = require('next-pwa')({
   dest: 'public',
   // disable: process.env.NODE_ENV === 'development',
-  register: true,
+  // register: true,
   // scope: '/app',
-  sw: 'service-worker.js',
+  //sw: 'service-worker.js',
   //...
 })
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,31 @@ import '../styles/ReactToastify.css'
 import type { AppProps } from 'next/app'
 import StoreProvider from '@contexts/StoreProvider'
 import { ToastContainer } from 'react-toastify'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+   useEffect(() => {
+      console.log('serviceWorker' in navigator)
+      if ('serviceWorker' in navigator) {
+         window.addEventListener('load', function () {
+            navigator.serviceWorker.register('/sw.js').then(
+               function (registration) {
+                  console.log(
+                     'Service Worker registration successful with scope: ',
+                     registration.scope
+                  )
+               },
+               function (err) {
+                  console.log('Service Worker registration failed: ', err)
+               }
+            )
+         })
+      }
+   }, [])
+
    return (
       <StoreProvider>
-         <ToastContainer/>
+         <ToastContainer />
          <Component {...pageProps} />
       </StoreProvider>
    )
