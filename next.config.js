85ea3e438573b1361569fa6a34b75fe336d928b0
brunozampieri1985 <brunozampieri1985@gmail.com>
2022-09-13 10:58:39 -0300

    feat: PWA

--- next.config.js
+++ next.config.js
@@ -4,4 +4,16 @@ const nextConfig = {
   swcMinify: true,
 }
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  // disable: process.env.NODE_ENV === 'development',
+  register: true,
+  // scope: '/app',
+  sw: 'service-worker.js',
+  //...
+})
+
+module.exports = withPWA({
+  nextConfig
+})
 module.exports = nextConfig
--- src/lib/components/PageLayout.tsx
+++ src/lib/components/PageLayout.tsx
@@ -2,14 +2,32 @@ import Head from 'next/head'
 import { ReactNode } from 'react'
 import Navbar from './Navbar'
 
-const PageLayout: React.FC<{ children: ReactNode }> = ({ children }) => {
+type PageLayoutProps = {
+   children: ReactNode
+   title?: string
+   description?: string
+}
+
+const PageLayout: React.FC<PageLayoutProps> = ({
+   children,
+   title,
+   description,
+}) => {
+
+   const metaTitle = title ?? 'Comida de Verdade'
+   const metaDesc = description ?? 'Comida de verdade em Peruíbe'
+   
+   const fullTitle = `Cozinha de Nutri - ${metaTitle}`
+   const fullDesc = `Cozinha de Nutri - ${metaDesc}`
+
    return (
       <div className="page-layout">
          <Head>
-            <title>Cozinha de Nutri - Comida de Verdade!</title>
+            <title>{fullTitle}</title>
+            <meta name="description" content={fullDesc} />
             <meta
-               name="description"
-               content="Cozinha de Nutri - Comida de verdade em Peruíbe"
+               name="viewport"
+               content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
             />
             <link rel="icon" href="/favicon.ico" />
          </Head>
