b764c23bfbf6b6d077a08198602aeeaf64f6f9ba
brunozampieri1985 <brunozampieri1985@gmail.com>
2022-09-14 08:24:50 -0300

    feat: Contact Form

--- src/lib/components/ContactForm.tsx
+++ src/lib/components/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from 'react'
+import { useState } from 'react'
 import Input from './Input'
 import Button from './Button'
 import validateForm from '@validation/Form'
