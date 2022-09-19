475bab43e25d418cfc634c5e8f5cca9bbc77a5df
brunozampieri1985 <brunozampieri1985@gmail.com>
2022-09-19 09:06:35 -0300

    feat: modal coponent

--- src/pages/descontos.tsx
+++ src/pages/descontos.tsx
@@ -2,68 +2,14 @@ import type { NextPage } from 'next'
 import PageLayout from '@components/PageLayout'
 import Button from '@components/Button'
 import Router from 'next/router'
+import DescontosCoponent from '@components/DescontosCoponent'
 
 const Descontos: NextPage = () => {
    return (
       <PageLayout title={'Descontos'}>
          <div className="info-page-wrapper">
             <div className="page-info">
-               <h1>Política de Descontos</h1>
-               <hr />
-               <br />
-               <p>
-                  Veja abaixo nossa tabela de descontos.{' '}
-                  <strong>APROVEITE!</strong>
-               </p>
-               <p>
-                  Basta adicionar os itens ao carrinho que o desconto é
-                  calculado automaticamente!
-               </p>
-               <div className="discount-table">
-                  <table>
-                     <thead>
-                        <th>Quantidade</th>
-                        <th>Desconto</th>
-                     </thead>
-                     <tbody>
-                        <tr>
-                           <td>
-                              Até <strong>6</strong> marmitas
-                           </td>
-                           <td>
-                              <strong>0%</strong>
-                           </td>
-                        </tr>
-                        <tr>
-                           <td>
-                              De <strong>7</strong> até <strong>13</strong>{' '}
-                              marmitas
-                           </td>
-                           <td>
-                              <strong>5%</strong>
-                           </td>
-                        </tr>
-                        <tr>
-                           <td>
-                              De <strong>14</strong> até <strong>20</strong>{' '}
-                              marmitas
-                           </td>
-                           <td>
-                              <strong>10%</strong>
-                           </td>
-                        </tr>
-                        <tr>
-                           <td>
-                              A partir de <strong>21</strong> marmitas
-                           </td>
-                           <td>
-                              <strong>15%</strong>
-                           </td>
-                        </tr>
-                     </tbody>
-                  </table>
-               </div>
-
+               <DescontosCoponent />
                <div>
                   <Button onClick={() => Router.push('/')}>
                      Voltar para Loja
@@ -74,5 +20,5 @@ const Descontos: NextPage = () => {
       </PageLayout>
    )
 }
-
+2
 export default Descontos
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import type { NextPage } from 'next'
+import { useEffect, useRef } from 'react'
 import PageLayout from '@components/PageLayout'
 import ProductList from '@components/ProductList'
 import Cart from '@components/Cart'
-import InstallButton from '@components/InstallButton'
+import Modal, { ModalRef } from '@components/Modal'
+import DescontosCoponent from '@components/DescontosCoponent'
 
 const Home: NextPage = () => {
+   const modalRef = useRef<ModalRef>(null)
+
+   useEffect(() => {
+      if (window !== undefined) modalRef.current?.open()
+   }, [])
+
    return (
-      <PageLayout>         
+      <PageLayout>
          <ProductList />
          <Cart />
+         <Modal closeOnOutsideClick ref={modalRef}>
+            <DescontosCoponent />
+         </Modal>
       </PageLayout>
    )
 }
